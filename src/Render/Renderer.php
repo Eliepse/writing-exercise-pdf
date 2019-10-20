dfeaefbb4ead82e08ed1d4a195070f2beedc1929
Eliepse <contact@eliepse.fr>
2019-10-20 21:42:40 +0800

    feat: add Caveat font

--- src/Render/Renderer.php
+++ src/Render/Renderer.php
@@ -4,11 +4,11 @@
 namespace Eliepse\WritingGrid\Render;
 
 
-use Eliepse\WritingGrid\Content\Page;
-use Eliepse\WritingGrid\Content\Word;
 use Eliepse\WritingGrid\Layout\BaseLayout;
 use Eliepse\WritingGrid\Utils\Math;
+use Eliepse\WritingGrid\Utils\Path;
 use Eliepse\WritingGrid\WordList;
+use Mpdf\Config\FontVariables;
 use Mpdf\Mpdf;
 use Mpdf\Output\Destination;
 
@@ -56,13 +56,31 @@ final class Renderer extends RenderElement
 			'margin_bottom' => Math::pxtomm(6),
 			'margin_header' => Math::pxtomm(12),
 			'margin_footer' => Math::pxtomm(6),
+			'fontdata' => $this->getFontDataConfig(),
 		];
+	}
+
+
+	private function getFontDataConfig(): array
+	{
+		return array_merge(
+			(new FontVariables())->getDefaults()['fontdata'],
+			[
+				'caveat' => [
+					'R' => 'Caveat-Regular.ttf',
+					'B' => 'Caveat-Bold.ttf',
+					'useOTL' => 0x03,
+				],
+			]
+		);
 	}
 
 
 	private function configure(): void
 	{
 		$this->mpdf->autoPageBreak = false;
+		$this->mpdf->AddFontDirectory(Path::resources('fonts/'));
+		$this->mpdf->SetDefaultFont('caveat');
 	}
 
 
